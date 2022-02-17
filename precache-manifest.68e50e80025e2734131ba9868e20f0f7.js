2997da5c80e8de4c3aeb1669fdb3d218d8faf8b7
4bizen <93602150+4bizen@users.noreply.github.com>
2022-02-17 09:53:20 +0200

    Update precache-manifest.68e50e80025e2734131ba9868e20f0f7.js

--- precache-manifest.68e50e80025e2734131ba9868e20f0f7.js
+++ precache-manifest.68e50e80025e2734131ba9868e20f0f7.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "124c9422585e9933d0401dac4833b617",
-    "url": "/pleditor/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "3f869e53284d5e4bc55f",
-    "url": "/pleditor/static/css/main.f4d59214.chunk.css"
+    "url": "/static/css/main.f4d59214.chunk.css"
   },
   {
     "revision": "73f36efa46d1917f37e7",
-    "url": "/pleditor/static/js/2.0a555bb2.chunk.js"
+    "url": "/static/js/2.0a555bb2.chunk.js"
   },
   {
     "revision": "b980a6607bae2171b6f1d2846f8c0b79",
-    "url": "/pleditor/static/js/2.0a555bb2.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.0a555bb2.chunk.js.LICENSE.txt"
   },
   {
     "revision": "3f869e53284d5e4bc55f",
-    "url": "/pleditor/static/js/main.a8938d32.chunk.js"
+    "url": "/static/js/main.a8938d32.chunk.js"
   },
   {
     "revision": "5e3e85da79714945161a",
-    "url": "/pleditor/static/js/runtime-main.66e8958b.js"
+    "url": "/static/js/runtime-main.66e8958b.js"
   }
 ]);
